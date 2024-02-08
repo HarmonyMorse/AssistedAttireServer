3d314eb27828e8992d5a4fa3d31dc9b1a89a3a93
HarmonyMorse <harmonythedev@gmail.com>
2024-02-08 08:43:02 -0800

    Add middleware to server.js

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ require('./config/db.connection.js')
 // Get port
 const { PORT } = process.env;
 
-// Import express
+// Import dependencies
 const express = require("express");
+const cors = require("cors")
+const morgan = require("morgan")
 
 // Create application object
 const app = express();
@@ -17,6 +19,11 @@ const app = express();
 const clothingItemsRouter = require("./routes/clothingItems.js");
 
 // Middleware
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+app.use(cors());
+app.use(morgan("dev"))
 
 // Routes
 // Test route
