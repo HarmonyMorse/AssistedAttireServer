3c0ee9b19d536c3c731585c8016d057cd2d6d964
HarmonyMorse <harmonythedev@gmail.com>
2024-02-08 14:48:18 -0800

    Add clothing items routes

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -6,16 +6,34 @@ const router = express.Router()
 
 // ROUTES
 
-// Clothing Item index route
+// Index (all clothing items)
 // GET /clothingItems
 router.get("/", async (req, res) => {
 	res.status(200).json({message: "clothing items index route"})
 });
 
-// Clothing Item create route
+// Create
 // POST /clothingItems
 router.post("/", async (req, res) =>  {
 	res.status(201).json({message: "clothing items create route"})
+});
+
+// Show (one clothing item)
+// GET /clothingItems/:id
+router.get("/:id", async (req, res) => {
+    res.status(200).json({message: "clothing items show route"})
+});
+
+// Update
+// PUT /clothingItems/:id
+router.put("/:id", async (req, res) => {
+    res.status(200).json({message: "clothing items update route"})
+});
+
+// Delete
+// DELETE /clothingItems/:id
+router.delete("/:id", async (req, res) => {
+    res.status(200).json({message: "clothing items delete route"})
 });
 
 module.exports = router
