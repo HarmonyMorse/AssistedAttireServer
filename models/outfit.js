01f8beb1dbaa113d63725b409b2b61ea25bd66cd
HarmonyMorse <harmonythedev@gmail.com>
2024-02-15 07:02:09 -0800

    Add Outfit model

---
+++ models/outfit.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const OutfitSchema = new Schema({
+    // userId: {
+    //     type: Number,
+    //     required: true,
+    //     default: 'testId',
+    // },
+    clothingItems: {
+        type: [{
+            type: mongoose.Types.ObjectId, ref: 'ClothingItem'
+        }],
+        default: [],
+    },
+    weather: {
+        type: String,
+        default: 'Unspecified',
+    },
+    occasion: {
+        type: String,
+        default: 'Unspecified',
+    },
+    notes: {
+        type: String,
+        default: '',
+    }
+});
+
+module.exports = mongoose.model('Outfit', OutfitSchema);
